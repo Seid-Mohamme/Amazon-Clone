4eb971522db7c6d562d6376680dcfcaed7ead373
Seid-Mohamme <seidmamme@gmail.com>
2025-06-02 21:11:50 +0300

    next com

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // local Instance of firebase function
-  // baseURL: "http://127.0.0.1:5001/clone-d1763/us-central1/api", // local Firebase functions
+  baseURL: "http://127.0.0.1:5001/clone-d1763/us-central1/api", // local Firebase functions
 
   // Deployment version of amazon server on render.com
   baseURL: "https://amazon-api-deploy-jspm.onrender.com",
