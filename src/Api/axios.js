67edd30dfaca8b39ef5b0519532742dc7f16b38a
Seid-Mohamme <seidmamme@gmail.com>
2025-06-02 19:33:14 +0300

    changes

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -5,11 +5,10 @@ export const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/clone-d1763/us-central1/api", // local Firebase functions
 
   // Deployment version of amazon server on render.com
-  // baseURL: "https://amazon-api-deploy-jspm.onrender.com",
+  baseURL: "https://amazon-api-deploy-jspm.onrender.com",
 
-  const baseURL=
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5001/clone-d1763/us-central1/api"
-    : "https://amazon-api-deploy-jspm.onrender.com"
-    // ✅ live backend
+  // const baseURL =
+  // import.meta.env.MODE === "development"
+  //   ? "http://localhost:5001/clone-d1763/us-central1/api"
+  //   : "https://your-backend-url.com"; // ✅ live backend here
 });
