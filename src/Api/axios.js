1b24342b9aa2c3563812e134d5dce2c8e3a90885
Seid-Mohamme <seidmamme@gmail.com>
2025-06-02 13:17:04 +0300

    last commit

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,10 +1,9 @@
-// import axios from "axios";
-// const axiosInstance = axios.create({
-//   baseURL: "http://127.0.0.1:5001/clone-d1763/us-central1/api",
-// });
-// export { axiosInstance };
 import axios from "axios";
 
 export const axiosInstance = axios.create({
+  // local Instance of firebase function
   baseURL: "http://127.0.0.1:5001/clone-d1763/us-central1/api", // local Firebase functions
+
+  // Deployment version of amazon server on render.com
+  baseURL: "https://amazon-api-deploy-jspm.onrender.com",
 });
